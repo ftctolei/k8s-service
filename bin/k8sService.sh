8a84e71dcb2673962898553fc868a28ed6d92660
leixichina <leixichina@126.com>
2022-01-17 14:49:00 +0800

    更新K8s-service 服务 启动|停止|状态 控制脚本

--- bin/k8sService.sh
+++ bin/k8sService.sh
@@ -11,6 +11,7 @@ JAR_CMD="/usr/bin/jar "
 JAVA_ARGS="-Xmx10g "
 JAR_MAIN_CLASS="cn.chinatelecom.kubernetes.StartService"
 
+
 displayHelp(){
     echo -e  ""
     echo -e  "Usage:  bash  $0  start|stop|status"
@@ -20,15 +21,37 @@ displayHelp(){
 }
 
 
+logPrint(){
+    log_type=${1:-"info"}
+    log_content=${2:-"no log content."}
+    case ${log_type} in
+    error)
+        echo -e "`date +'%F %H:%M:%S'`[ERROR] ${log_content}";;
+    info)
+        echo -e "`date +'%F %H:%M:%S'`[INFO] ${log_content}";;
+    warn)
+        echo -e "`date +'%F %H:%M:%S'`[WARN] ${log_content}" ;;
+    debug)
+        echo -e "`date +'%F %H:%M:%S'`[DEBUG] ${log_content}" ;;
+    esac
+}
+
+
 startService(){
     pid=`/bin/ps  -ef |grep  ${jar_file} |grep -v grep  |awk -F ' ' '{print $2}'`
     if [[ "${pid}"  != "" ]];then
-        echo "服务已在运行, 请先停止."
+        logPrint "服务已在运行, 请先停止."
         statusService
         exit 0
     fi
-    ${JAR_CMD}  -uf   ${jar_file}   ${config_properties}
-    ${JAR_CMD}  -uf   ${jar_file}   ${log4j_properties}
+    if [[ -f  ${config_properties} ]];then
+        logPrint "加载配置文件: ${config_properties}"
+        ${JAR_CMD}  -uf   ${jar_file}   ${config_properties}
+    fi
+    if [[ -f  ${log4j_properties} ]];then
+         logPrint "加载配置文件: ${log4j_properties}"
+        ${JAR_CMD}  -uf   ${jar_file}   ${log4j_properties}
+    fi
     nohup ${JAVA_CMD}  ${JAVA_ARGS}  -jar  ${jar_file}  ${JAR_MAIN_CLASS} > /dev/null 2>&1 &
 }
 
@@ -36,7 +59,7 @@ startService(){
 stopService(){
     /bin/ps  -ef |grep  ${jar_file} |grep -v grep
     pid=`/bin/ps  -ef |grep  ${jar_file} |grep -v grep  |awk -F ' ' '{print $2}'`
-    echo "PID: ${pid}"
+    logPrint "PID: ${pid}"
     if [[ "${pid}" != "" ]] ;then
         /bin/kill  -9  ${pid}
     fi
@@ -44,13 +67,13 @@ stopService(){
 
 
 statusService(){
-    echo "/bin/ps  -ef |grep  ${jar_file} |grep -v grep"
+    logPrint "/bin/ps  -ef |grep  ${jar_file} |grep -v grep"
     /bin/ps  -ef |grep  ${jar_file} |grep -v grep
     pid=`/bin/ps  -ef |grep  ${jar_file} |grep -v grep  |awk -F ' ' '{print $2}'`
     if [[ "${pid}"  != "" ]];then
-        echo "[runing]服务正在运行, PID: ${pid}"
+        logPrint "[runing]服务正在运行, PID: ${pid}"
     else
-        echo "[NOT runing]未发现服务进程."
+        logPrint "[NOT runing]未发现服务进程."
     fi
     echo  -e ""
 }
@@ -64,17 +87,17 @@ fi
 op="$1"
 case ${op} in
     start)
-        echo "Start K8sService ..."
+        logPrint "Start K8sService ..."
         startService
-	echo "finished ."
+	    logPrint "finished ."
         ;;
     stop)
-        echo "Stop K8sService ..."
+        logPrint "Stop K8sService ..."
         stopService
-	echo "finished ."
+	    logPrint "finished ."
         ;;
     status)
-        echo "K8sService status, (ps -ef)"
+        logPrint "K8sService status, (ps -ef)"
         statusService
         ;;
      *)
