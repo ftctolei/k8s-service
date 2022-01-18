3d292de62965e3fa56dcdbf30dab76905e24a263
leixichina <leixichina@126.com>
2022-01-18 13:49:17 +0800

    update jetty config, close ServerVersion display to imporve safety.

--- src/main/java/cn/kubernetes/service/rest/JettyServer.java
+++ src/main/java/cn/kubernetes/service/rest/JettyServer.java
@@ -1,7 +1,10 @@
 package cn.kubernetes.service.rest;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ErrorPageErrorHandler;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
@@ -19,26 +22,33 @@ public class JettyServer {
     private static final Logger log = LoggerFactory.getLogger(JettyServer.class);
 
     public void start(int port) throws Exception {
-        Server server = new Server(port);
+        //Server server = new Server(port);
+        Server server = new Server();
 
         ServletHolder servletHolder = new ServletHolder(org.glassfish.jersey.servlet.ServletContainer.class);
-        Map<String, String> parameterMap = new HashMap<>(7);
+        Map<String, String> parameterMap = new HashMap<>(8);
         parameterMap.put("jersey.config.server.provider.packages", "cn.kubernetes.service.rest.resource");
         parameterMap.put("jersey.config.beanValidation.enableOutputValidationErrorEntity.server", "true");
         parameterMap.put("jersey.api.json.POJOMappingFeature", "true");
         parameterMap.put("jersey.config.property.resourceConfigClass", "jersey.api.core.PackagesResourceConfig");
         parameterMap.put("org.glassfish.jersey.logging.level", "FINE");
         parameterMap.put("java.util.logging.ConsoleHandler.level", "FINE");
+
         // 添加对POST请求中MultiPart的支持, 如上传文件等.
         parameterMap.put("jersey.config.server.provider.classnames", "org.glassfish.jersey.media.multipart.MultiPartFeature");
 
         servletHolder.setInitParameters(parameterMap);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.addServlet(servletHolder, "/*");
-        ErrorPageErrorHandler errorHandler = new ErrorPageErrorHandler();
-        errorHandler.addErrorPage(404, "html/404.html");
-        context.setErrorHandler(errorHandler);
         server.setHandler(context);
+        context.addServlet(servletHolder, "/*");
+
+        // 设置jetty9出错时不返回版本号,提高安全性
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSendServerVersion(false);
+        HttpConnectionFactory httpFactory = new HttpConnectionFactory(httpConfig);
+        ServerConnector httpConnector = new ServerConnector(server, httpFactory);
+        server.setConnectors(new Connector[]{httpConnector});
+        httpConnector.setPort(port);
 
         server.start();
         server.join();
